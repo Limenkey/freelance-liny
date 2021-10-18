f71b0795a88c93c45715a7d83f85bd6cf150c20d
limenkey <renlimenki@gmail.com>
2021-10-18 18:49:27 +0300

    Footer and header height adjustments

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/img-redundant-alt */
-
 import { useEffect } from "react"
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
